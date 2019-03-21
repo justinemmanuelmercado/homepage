486804ea8735354201aa048e04ea07d13d7da7fd
EJ Mercado <ej@ejmercado.com>
2019-03-22 07:37:00 +0800

    add typography. bug with eslint not picking up types

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import typography from "./typography";
 // Components
+import { Helmet } from "react-helmet";
+// import { TypographyStyle, GoogleFont } from "react-typography";
 import { Layout } from "./components/Layout";
 import { Favorites } from "./components/Favorites";
 import { Bookmarks } from "./components/Bookmarks";
-
 class App extends Component {
-  public render(): React.ReactElement {
-    return (
-      <Layout>
-        <div className="section">
-          <Favorites />
-        </div>
-        <div className="section">
-          <Bookmarks />
-        </div>
-      </Layout>
-    );
-  }
+    public render(): React.ReactElement {
+        return (
+            <Layout>
+                <Helmet>
+                    <style type="text/css">{typography}</style>
+                </Helmet>
+                <div className="section">
+                    <Favorites />
+                </div>
+                <div className="section">
+                    <Bookmarks />
+                </div>
+            </Layout>
+        );
+    }
 }
 
 export default App;
---
+++ src/typography.ts
@@ -0,0 +1,11 @@
+import Typography from "typography";
+import moraga from "typography-theme-moraga";
+
+const typography = new Typography(moraga);
+export default typography.toString();
+
+// Output CSS as string.
+// export default typography.toString()
+
+// Or insert styles directly into the <head> (works well for client-only
+// JS web apps.
