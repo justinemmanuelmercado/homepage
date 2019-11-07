aebddbe1f9e36b9cc9bc8b6a892958e3b28bf7b5
EJ Mercado <ej@ejmercado.com>
2019-11-08 00:27:40 +0800

    modify script to use environment variables

--- backupdb.sh
+++ backupdb.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-DUMP_NAME=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-CONTAINER_NAME=backend_mydb_1
-FOLDER_ID=1JfDJEDjZ9HAbMU1_0B6s4zcwxJ5-UD_i
-
+export $(egrep -v '^#' .env | xargs)
 docker exec -t $CONTAINER_NAME pg_dumpall -c -U links -l links > $DUMP_NAME
 gdrive-linux-x64 upload --parent $FOLDER_ID $DUMP_NAME
