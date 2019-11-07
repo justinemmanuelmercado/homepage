ebabafacfd12231f77798a672ea9cad717bf294d
EJ Mercado <ej@ejmercado.com>
2019-11-08 01:19:21 +0800

    changes to backup script

--- backupdb.sh
+++ backupdb.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 DUMP_NAME=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 export $(egrep -v '^#' .env | xargs)
-docker exec -t $CONTAINER_NAME pg_dumpall -c -U links -l links > $DUMP_NAME
-gdrive-linux-x64 upload --parent $FOLDER_ID $DUMP_NAME
+/usr/bin/docker exec -t $CONTAINER_NAME pg_dumpall -c -U links -l links > $DUMP_NAME
+/usr/local/bin/gdrive-linux-x64 upload --parent $FOLDER_ID $DUMP_NAME
