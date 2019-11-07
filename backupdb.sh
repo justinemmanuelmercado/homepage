ffc54bbd81db91c4b6e79a3d298b610d1162c722
EJ Mercado <ej@ejmercado.com>
2019-11-08 00:34:49 +0800

    minor fix

--- backupdb.sh
+++ backupdb.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+DUMP_NAME=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 export $(egrep -v '^#' .env | xargs)
 docker exec -t $CONTAINER_NAME pg_dumpall -c -U links -l links > $DUMP_NAME
 gdrive-linux-x64 upload --parent $FOLDER_ID $DUMP_NAME
